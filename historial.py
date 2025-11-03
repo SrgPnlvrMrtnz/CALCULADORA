6615bbeb76f7cf751fbe0cc8b54d865d80076c8b
Sergio <4108546@alu.murciaeduca.es>
2025-11-03 20:59:38 +0100

    historial completo

--- historial.py
+++ historial.py
@@ -1,20 +1,23 @@
 palabras = []
 exit = False
 
+print("Escribre una palabra, si escribes la palabra 'historial', verás el historial, si solo quieres los tres últimos elementos, escribe 'ultimos', deja en blanco para finalizar")
+
 while exit == False:
 
-    palabra = input("Escribre una palabra, si escribes la palabra 'historial', verás el historial")
+    palabra = input("Escribra su palabra: ")
 
-    
     if (palabra == ""):
 
         exit = True
 
-    else: 
-        
-        palabras.append(palabra)
-
-    if (palabra.upper() == "historial".upper()):
+    elif (palabra.upper() == "historial".upper()):
 
         print("Historial: " , palabras)
+    
+    elif (palabra.upper() == "ultimos".upper()):
+
+        print("Historial de los últimos 3 elementos: " , palabras[-3] , " " , palabras[-2] , " " , palabras[-1] , " ")
+    else:
+        palabras.append(palabra)   
 
